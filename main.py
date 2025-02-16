2f8b973d66590e0b19db692ee7d72c8c4818ba86
Ekaterina Levkova <mishinacaterina@yandex.ru>
2025-02-16 17:22:40 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from application.salary import calculate_salary
+from application.db.people import get_employees
+
+if __name__ == "__main__":
+    # Вызов функции для получения списка сотрудников
+    employees = get_employees()
+    
+    # Для каждого сотрудника вызываем функцию расчета з/п
+    for employee in employees:
+        print(f"Рассчитываем зарплату для сотрудника: {employee}")
+        calculate_salary(employee)
---
+++ salary.py
@@ -0,0 +1,3 @@
+def calculate_salary(employee):
+    # Расчета зарплаты
+    print(f"Зарплата для сотрудника {employee} рассчитана.")
