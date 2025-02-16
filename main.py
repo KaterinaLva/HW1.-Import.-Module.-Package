36752c9e6a39c289539aa20a90c1291182745c31
Ekaterina Levkova <mishinacaterina@yandex.ru>
2025-02-16 17:44:56 +0300

    Adding datetime

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import datetime
 from application.salary import calculate_salary
 from application.db.people import get_employees
 
 if __name__ == "__main__":
+    urrent_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    
     # Вызов функции для получения списка сотрудников
     employees = get_employees()
     
